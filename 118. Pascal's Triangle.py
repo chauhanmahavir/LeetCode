25a08b80107f458ca9db464b6aae8c5716e2ed8f
Chauhan Mahaveer <45894858+chauhanmahavir@users.noreply.github.com>
2023-06-12 14:45:32 -0400

    Update 118. Pascal's Triangle.py

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -8,9 +8,7 @@ class Solution:
                     if j == 0 or j == i-1:
                         sub_list.append(1)
                     else:
-                        print(i, j)
                         sub_list.append(default_list[i-1][j] + default_list[i-1][j-1])
-                print(sub_list)
                 default_list.append(sub_list)
             return default_list[1:]
         else:
